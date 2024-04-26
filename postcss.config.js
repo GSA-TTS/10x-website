1686e40b1c4c1447bd5750aef9d50650bf7fad5f
Jonathan Bobel <jonathan.bobel@gsa.gov>
2024-04-26 13:03:58 -0400

    Trying out PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
-  plugins: [require('autoprefixer')({
-    map: process.env.ELEVENTY_ENV !== 'production'
-  })],
+  plugins: [
+    require('autoprefixer')({
+      map: process.env.ELEVENTY_ENV !== 'production'
+    }),
+    require('@fullhuman/postcss-purgecss')({
+      content: ["./_site/**/*.html"],
+      css: ["./_site/**/*.css"],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
+  ],
 };
