5059fbbdede480bd7e2d8a0e3806ba3ed888389a
ethangardner <ethan.gardner@gsa.gov>
2024-07-19 11:35:58 -0400

    add purgecss options file and leave a placeholder for it to be brought into the postcss config

---
+++ config/purgecss-options.js
@@ -0,0 +1,26 @@
+const purgeCSSOptions = {
+    css: ["./_site/assets/styles/*.css"],
+    content: [
+        "./_site/**/*.{html,js,svg}",
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+    safelist: [
+        "abbr[title]",
+        "kbd",
+        "samp",
+        "sub",
+        "optgroup",
+        "[type=reset]",
+        "[type=reset]::-moz-focus-inner",
+        "[type=reset]:-moz-focusring",
+        "fieldset",
+        "[type=radio]",
+        "summary",
+        "[contentEditable=true]:focus",
+        "cite",
+        "dfn",
+        "pre",
+    ]
+};
+
+module.exports = purgeCSSOptions;
